9c58bcf8e864ab1bc2617c26aebac9900ff434c5
NWalter74 <nicole.walter@iths.se>
2022-10-31 06:44:18 +0100

    .

---
+++ Labb3Bordsbokning/BokningsDag.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb3Bordsbokning
+{
+    public class BokningsDag
+    {
+        public string datum { get; private set; }
+        public List<string> tidLista = new List<string>() { "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00"};
+    }
+}
